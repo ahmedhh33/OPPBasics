bac21894562a6d1a41a51862b4f57c8154a554e5
ahmedhh33 <ahmedhh3633@gmail.com>
2023-06-13 14:43:18 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace OOPBasics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student std1=new Student(1,"Mohammed","A","1020"); // creating a bjects
+            //std1.id = 1;
+            //std1.name = "Mohammed";
+            //std1.grade = "A";
+
+            //Console.WriteLine("Std1 Details "); 
+            //Console.WriteLine("Name : "+ std1.name);//printing  objects
+            //Console.WriteLine("Grade :"+ std1.grade);
+            //std1.stusy(); // calling the method which show what object can do 
+
+            Student std2 = new Student(2, "Umran","A+","1090"); // creating a bjects
+            //std2.id = 123;
+            //std2.name = "Omran";
+            //std2.grade = "A+";
+            //std2.name = "test";
+
+            Console.WriteLine("Std2 Details ");
+            Console.WriteLine("Name : " + std2.name);//printing  objects
+            Console.WriteLine("Grade :" + std2.grade);
+            std2.stusy(); // calling the method which show what object can do 
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,30 @@
+﻿
+
+namespace OOPBasics
+{
+     class Student
+    {
+       public long id;// properties
+       public string name;
+       public string grade;
+       public string regestrationNumber;
+       public Student(long id, string name,string grade, string regestrationNumber) // constructore same name as class and it intialise proberties
+        {
+            this.id = id;//this return to class properties
+            this.name = name;
+            this.grade = grade;
+            this.regestrationNumber = regestrationNumber;
+            printStuentDetails();
+
+        }
+        public void stusy()// method
+        {
+            Console.WriteLine("studyint "+name+" is studying : ");
+        }
+
+        void printStuentDetails()
+        {
+            Console.WriteLine("Name : "+this.name+" Registration number : "+this.regestrationNumber);
+        }
+    }
+}
